0f96cd780d49ccedd4a5c818f46f11e56a16e6fc
mennah4 <mennahjafar@hotmail.com.com>
2024-04-17 20:58:56 +0300

    Array transformation optimization

--- algorithms/arrayTransformation.js
+++ algorithms/arrayTransformation.js
@@ -1,22 +1,27 @@
-// Returns a complixity of o(n*m) due to the nested while loop
+// Returns a complixity of O(n log n) due to the hash map
 
-function arrayTransformation(arr) {
-    console.time('arrayTransformation');
-  for (let i = 0; i < arr.length; i++) {
-    while (arr[i] !== 1) {
-      if (arr[i] % 2 === 0) {
-        arr[i] /= 2;
-      } else {
-        arr[i] = arr[i] * 3 + 1;
-      }
+function transformArray(arr) {
+  const map = new Map();
+
+  function transform(n) {
+    if (n === 1) return 1;
+    if (map.has(n)) return map.get(n);
+
+    if (n % 2 === 0) {
+      map.set(n, transform(n / 2));
+    } else {
+      map.set(n, transform(n * 3 + 1));
     }
+
+    return map.get(n);
   }
-  console.timeEnd('arrayTransformation');
-  return arr;
+
+  return arr.map(transform);
 }
 
 
-console.log(arrayTransformation([6, 2, 3, 4]));
-console.log(arrayTransformation([1, 2, 3, 4]));
-console.log(arrayTransformation([1, 2, 3, 4, 5]));
-console.log(arrayTransformation([1, 2, 3, 4, 5, 6]));
+
+console.log(transformArray([6, 12, 3, 4]));
+console.log(transformArray([1, 2, 3, 4]));
+console.log(transformArray([1, 2, 3, 4, 5]));
+console.log(transformArray([1, 2, 3, 4, 5, 6]));
